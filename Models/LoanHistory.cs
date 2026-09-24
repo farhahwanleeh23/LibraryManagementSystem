b05e7a4a9a06f7c9cf1fb1b99546d702489d6318
farhahwanleeh23 <farhahwanleeh@gmail.com>
2026-09-24 18:15:40 +0800

    Update LoanHistory.cs

--- Models/LoanHistory.cs
+++ Models/LoanHistory.cs
@@ -1,1 +1,28 @@
+using System;
+
+namespace LibraryManagementSystem.Models
+{
+    public class LoanHistory
+    {
+      public Guid LoanId { get; private set; }
+      public Guid BookId { get; private set; }
+      public Guid MemberId { get; private set; }
+      public DateTime IssueDate { get; private set; }
+      public DateTime DueDate { get; private set; }
+      public DateTime? ReturnDate { get; private set; }
+      public string Status { get; private set; } //Active, Returned, Overdue
+
+      public LoanHistory(Guid bookCopyId, Guid memberId)
+      {
+        int loanDurationDays = 14;
+        LoanId = Guid.NewGuid();
+        BookId = bookId;
+        MemberId = memberId;
+        IssueDate = DateTime.UtcNow;
+        DueDate = DateTime.UtcNow.AddDays(loanDurationDays);
+        ReturnDate = null;
+        Status = "Active";
+      }
+    }
+}
 
